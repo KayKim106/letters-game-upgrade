8550ebc6f0d7629b2850cd3f6d4c51682959d04c
unknown <kaykim106@gmail.com>
2019-09-18 02:08:26 -0400

    adds client(letter game base) & shuffle function

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const data = require('./client/src/data/test-board-1.json')
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
+app.get('/shuffle', async (req,res)=>{
+    let i, j ,temp, letterData;
+    letterData = data.board;
+    i = letterData.length;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = letterData[j];
+        letterData[j] = letterData[i]
+        letterData[i]= temp;
+    }
+    res.send(letterData);
 })
 // Server Port following by env file setting
 // or use server port as 5000
